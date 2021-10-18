aa1b2b46f0296ef4a6dbe172642a6f2c75c3533c
Miguel Dinis de Sousa <miguel.dinis.s@tecnico.ulisboa.pt>
2021-10-18 15:15:55 +0100

    oneJoint function added

---
+++ KAP/matlab/oneJoint.m
@@ -0,0 +1,28 @@
+function resultList = oneJoint(numBody1, numBody2, origin, cm1, jointPosition, cm2)
+%myFun - Description
+%
+% Syntax: output = myFun(input)
+%
+% Long description
+    if length(jointPosition) == (2) &  length(cm1) == 2 & length(cm2) == 2 & length(origin) == 2 
+        resultList = [numBody1, numBody2];
+        cm1 = cm1 - origin;
+        cm2 = cm2 - origin;
+        jointPosition = jointPosition - origin;
+
+        minusBody1 = jointPosition - cm1;
+        minusBody2 = jointPosition - cm2;
+
+        distance1 = norm(minusBody1);
+        distance2 = norm(minusBody2);
+
+        resultList = [resultList, distance1, 0, -distance2, 0];
+
+    else
+        disp('Verify the lenght of the inputed Vectors, these are wrong');
+        
+    end
+
+    
+
+end
