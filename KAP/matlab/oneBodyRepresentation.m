9a5a3cfdeb40254a535c93fa7eb4d13a7a3787da
Miguel Dinis de Sousa <miguel.dinis.s@tecnico.ulisboa.pt>
2021-10-19 14:25:37 +0100

    Added catalogue of Cm's and θ as well as ξ

--- KAP/matlab/oneBodyRepresentation.m
+++ KAP/matlab/oneBodyRepresentation.m
@@ -1,17 +1,32 @@
-function bodyAxis = oneBodyRepresentation(joint1XVector, joint1ZVector, joint2XVector, joint2ZVector)
+function bodyAxis, timeTheta, timeCm = oneBodyRepresentation(joint1XVector, joint1ZVector, joint2XVector, joint2ZVector, bodyLength, bodyCm)
 %oneBodyRepresentation - All vectors are positions througout time, these positions must be normalized to the origin lAnkle(0)
 %
 % Syntax: output = myFun(input)
 %
 % Long description
     
-bodyAxis = zeros(length(joint1XVector)); %% Prelocating the vector for speed
+bodyAxis{length(joint1XVector), 1} = {[]}; %% Prelocating the vector for speed
 
-for frame = 1:length(joint1XVector)
-    stick = [joint1XVector(frame), joint1ZVector(frame)]-[joint2XVector(frame), joint2ZVector(frame)]; %Defines the body our stick as a vector from the distal to the proximal joint
+timeCm{length(joint1XVector), 1} = {[]};
 
-    bodyAxis(frame) = (stick/(transpose(stick)*stick));   %%%This will hold all axis positions throughout each frame, bodyAxis(t) == ξ(t)
-end
+Global 
+
+    for frame = 1:length(joint1XVector)
+        stick = [joint1XVector(frame), joint1ZVector(frame)]-[joint2XVector(frame), joint2ZVector(frame)]; %Defines the body our stick as a vector from the distal to the proximal joint
+
+        bodyAxis{frame,1} = (stick/(transpose(stick)*stick));   %%%This will hold all axis positions throughout each frame, bodyAxis(t) == ξ(t)
+
+        timeCm{frame,1} = [joint1XVector(frame),joint1ZVector(frame)] + bodyCm*bodyLength*bodyAxis(frame); %%Updating the CM of our body throughout ti
+        
+        if bodyAxis{frame,1}(1) >= 0
+            theta(frame) = atan(bodyAxis{frame,1}(1)/bodyAxis{frame,1}(2))
+
+        else
+            theta(frame) = atan(bodyAxis{frame,1}(1)/bodyAxis{frame,1}(2)) + pi
+        end
+
+    end
 
 
-end
+
+end         
