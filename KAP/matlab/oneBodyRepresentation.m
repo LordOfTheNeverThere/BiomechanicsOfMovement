4551870db8aafa9ea6b86428c594e7f12b7edda1
Miguel Dinis de Sousa <miguel.dinis.s@tecnico.ulisboa.pt>
2021-10-20 20:47:40 +0100

    Fixed CM and Theta Acquisition

--- KAP/matlab/oneBodyRepresentation.m
+++ KAP/matlab/oneBodyRepresentation.m
@@ -1,29 +1,46 @@
-function bodyAxis, timeTheta, timeCm  = oneBodyRepresentation(joint1XVector, joint1ZVector, joint2XVector, joint2ZVector, bodyLength, bodyCm)
-%oneBodyRepresentation - All vectors are positions througout time, these positions must be normalized to the origin lAnkle(0)
+function bodyAxis, timeTheta, timeCm  = oneBodyRepresentation(body)
+%oneBodyRepresentation - receives a body from the BodySelector and will evaluate its Position and angle throughout time
 %
 % Syntax: output = myFun(input)
 %
 % Long description
     
-bodyAxis{length(joint1XVector), 1} = {[]}; %% Prelocating the vector for speed
+bodyLength = body.size; %Lenght of the Body
+bodyCm = body.cmVector; %Initial CM vector
+cmFromProx = body.cmFromProx; %Percentage of the lenght the CM is from the Proximal Joint
+joint1XVector = body.xPjoint; % x position of the Proximal Joint
+joint1ZVector = body.zPjoint; % z position of the Proximal Joint
+joint2XVector = body.xDjoint; % x position of the Distal Joint
+joint2ZVector = body.zDjoint; % z position of the Distal Joint
 
+bodyAxis{length(joint1XVector), 1} = {[]}; % Prelocating the vectors for speed
+timeTheta = zeros(length(joint1XVector));
 timeCm{length(joint1XVector), 1} = {[]};
+fixedFrameCM = [-bodyLength*cmFromProx,0]
 
-Global 
 
     for frame = 1:length(joint1XVector)
         stick = [joint1XVector(frame), joint1ZVector(frame)]-[joint2XVector(frame), joint2ZVector(frame)]; %Defines the body our stick as a vector from the distal to the proximal joint
 
-        bodyAxis{frame,1} = (stick/(transpose(stick)*stick));   %%%This will hold all axis positions throughout each frame, bodyAxis(t) == ξ(t)
-
-        timeCm{frame,1} = [joint1XVector(frame),joint1ZVector(frame)] + bodyCm*bodyLength*bodyAxis(frame); %%Updating the CM of our body throughout ti
+        bodyAxis{frame,1} = (stick/sqrt((transpose(stick)*stick)));   %%%This will hold all axis positions throughout each frame, bodyAxis(t) == ξ(t)
         
-        if bodyAxis{frame,1}(1) >= 0
-            theta(frame) = atan(bodyAxis{frame,1}(1)/bodyAxis{frame,1}(2));
+        if bodyAxis{frame,1}(1) >= 0 & bodyAxis{frame,1}(2) >= 0
+
+            timeTheta(frame) = atan(bodyAxis{frame,1}(1)/bodyAxis{frame,1}(2));
+
+        elseif bodyAxis{frame,1}(1) >= 0 & bodyAxis{frame,1}(2) < 0
+
+            timeTheta(frame) = atan(bodyAxis{frame,1}(1)/bodyAxis{frame,1}(2)) + 2*pi;
 
         else
-            theta(frame) = atan(bodyAxis{frame,1}(1)/bodyAxis{frame,1}(2)) + pi;
+
+            timeTheta(frame) = atan(bodyAxis{frame,1}(1)/bodyAxis{frame,1}(2)) + pi;
         end
+        lameMatrix = [cos(timeTheta(frame)), -sin(timeTheta(frame)); ....
+                        sin(timeTheta(frame)), cos(timeTheta(frame))];
+
+
+        timeCm{frame,1} = [joint1XVector(frame),joint1ZVector(frame)] - lameMatrix*fixedFrameCM; %%Updating the CM of our body throughout time with the vector that point to the cm in the local frame
 
     end
 
