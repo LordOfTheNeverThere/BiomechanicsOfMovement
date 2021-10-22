6c80d765fa645527828ee554a2836134eea8fe51
Maria Brazão <maria.brazao@tecnico.ulisboa.pt>
2021-10-22 01:09:27 +0100

    ProcessData finished

--- KAP/matlab/ProcessData.m
+++ KAP/matlab/ProcessData.m
@@ -7,8 +7,10 @@ function [final_fc] = ProcessData(fs,coord)
 clc
 file = readtsvCustom("trial_0011_g05.tsv");
 fc = (0.1:0.1:10);
-x = coord{:,1}; %posição da coordenada x no file
-z = coord{:,2}; %posição da coordenada z no file
+global origin
+origin = [file{1,30}, file{1,32}];
+x = coord(:,1) - origin(1); %posição da coordenada x no file
+z = coord(:,2) - origin(2); %posição da coordenada z no file
 % xf = zeros(length(x),1);
 % zf = zeros(length(z),1);
 Rx = zeros(length(fc),1);
@@ -16,47 +18,57 @@ Rz = zeros(length(fc),1);
 f = 1;
 while f <= length(fc)
     [b,a] = butter(2,(2 * fc(f))/fs, 'low');
-    xf = filtfilt(b,a,x);    
-    zf = filtfilt(b,a,z);
-    Rx(f) = sqrt(sum((x(:) - xf(:)).^2)/length(x)^2);
-    Rz(f) = sqrt(sum((z(:) - zf(:)).^2)/length(z)^2);
+    xf(:,f) = filtfilt(b,a,x);    
+    zf(:,f) = filtfilt(b,a,z);
+    Rx(f) = sqrt(sum((x(:) - xf(:,f)).^2)/length(x)^2);
+    Rz(f) = sqrt(sum((z(:) - zf(:,f)).^2)/length(z)^2);
     f = f + 1;
 end
 
 final_fc = zeros(1,2);
 minCorr = 0.97; %slides
 rx = 1;
-stop_signx = false; 
-while rx < length(fc) && stop_signx
+while rx <= length(fc) 
     [xCorrelation, ~ , bx] = regression(fc(rx:length(fc)),Rx(rx:length(fc))');
     xCorrelation = xCorrelation^2;
     if xCorrelation > minCorr
-        bfx = bx;
-        stop_signx = true;
-        xi = intersect(Rx,bx);
-        final_fc(1,1) = xi; 
+        break
     else
-        rx = rx + 2;
+        rx = rx + 1;
     end
 end
 
-stop_signz = false;
+
 rz=1;
-while rz < length(fc) && stop_signz
-    [zCorrelation, ~ , bz] = regress(fc(rz:length(fc)),Rx(rz:length(fc))');
+while rz <= length(fc) 
+    [zCorrelation, ~ , bz] = regression(fc(rz:length(fc)),Rz(rz:length(fc))');
     zCorrelation = zCorrelation^2;
     if zCorrelation > minCorr
-        bfz = bz;
-        stop_signz = true;
-        zi = intercept(Rz,bz);
-        final_fc(1,2) = zi;
+        break
     else
-        rz = rz + 2;
+        rz = rz + 1;
     end
 end
 
+% disp(bz);
+% disp(bx);
+% disp("RX =" + rx);
+% disp("RZ =" + rz);
+Rx_temp = Rx - bx;
+Rz_temp = Rz - bz;
+% disp("Rx =" + Rx);
+% disp("Rz =" + Rz);
+% disp("Rx_temp=" + Rx_temp);
+%disp("Rz_temp" + Rz_temp);
+min_value_x = min(abs(Rx_temp));
+%disp("ABS=" + min(abs(Rx_temp)));
+min_value_z = min(abs(Rz_temp));
+% disp("MINVALUE= " + min_value_x);
+index_x = find(abs(Rx_temp) == min_value_x);
+index_z = find(abs(Rz_temp) == min_value_z);
+final_fc(1,1) = fc(index_x);
+final_fc(1,2) = fc(index_z);
 
-disp(final_fc);
 end
 
 
