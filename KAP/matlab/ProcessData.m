265947fc3e0e1b5cae821ca10b65f6a3ef2ec27b
Maria Brazão <maria.brazao@tecnico.ulisboa.pt>
2021-10-23 13:45:11 +0100

    .

--- KAP/matlab/ProcessData.m
+++ KAP/matlab/ProcessData.m
@@ -6,7 +6,12 @@
 function [final_fc, filtered_coordinates] = ProcessData(fs,coord)
 clc;
 fc = (0.1:0.1:10);
+<<<<<<< Updated upstream
 global origin;
+=======
+global origin
+origin = [file.(33)(1),file.(35)(1)];
+>>>>>>> Stashed changes
 x = (coord(:,1) - origin(1))/1000; %posição da coordenada x no file
 z = (coord(:,2) - origin(2))/1000; %posição da coordenada z no file
 % xf = zeros(length(x),1);
@@ -64,7 +69,13 @@ final_fc(1,1) = fc(index_x);
 final_fc(1,2) = fc(index_z);
 filtered_coordinates(:,1) = xf(:,index_x);
 filtered_coordinates(:,2) = zf(:,index_z);
+<<<<<<< Updated upstream
 
+=======
+disp(filtered_coordinates);
+% disp("XF= " + xf);
+% disp("ZF= " + zf);
+>>>>>>> Stashed changes
 end
 
 
