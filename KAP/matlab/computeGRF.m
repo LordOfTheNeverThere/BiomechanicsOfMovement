ef3165bf7266d1901512f4e86735d307b63074bb
Miguel Dinis de Sousa <miguel.dinis.s@tecnico.ulisboa.pt>
2021-11-03 11:18:43 +0000

    Improved Code for filtering

--- KAP/matlab/computeGRF.m
+++ KAP/matlab/computeGRF.m
@@ -9,15 +9,36 @@ function output = myFun(input)
 
     [forcePlate1, forcePlate2, forcePlate3] = tsv2mat(0,0,0);
     
+
+    
+    
+    %% getting rid of the y data %%
+%     forcePlate1(:,1:2) = forcePlate1(:,1:2:3);
+%     forcePlate2(:,1:2) = forcePlate2(:,1:2:3);
+%     forcePlate3(:,1:2) = forcePlate3(:,1:2:3);
+%     
+%     forcePlate1(:,3:4) = forcePlate1(:,4:2:6)/1000;
+%     forcePlate2(:,3:4) = forcePlate2(:,4:2:6)/1000;
+%     forcePlate3(:,3:4) = forcePlate3(:,4:2:6)/1000;
+    disp(forcePlate1);
+    %Remove unused colunms
+    forcePlate1(:,2:3:5) = [];
+    forcePlate2(:,2:3:5) = [];
+    forcePlate3(:,2:3:5) = [];
+    
+    forcePlate1(:,3:4) = forcePlate1(:,3:4)/1000;
+    forcePlate2(:,3:4) = forcePlate2(:,3:4)/1000;
+    forcePlate3(:,3:4) = forcePlate3(:,3:4)/1000;
+    
+    disp(forcePlate1);
+    
     forcePlates = {forcePlate1, forcePlate2, forcePlate3};
-    
-    
     for forcePlateIndex = 1:length(forcePlates)
         forcePlate = forcePlates{1,forcePlateIndex};
         contactTimeSteps = find(forcePlate(:,1) > 5);
         
         %% Pre-Filtering Forces %%
-        for col = 1:2:3
+        for col = 1:2
 
             for row = 1:length(forcePlate1(:,1))
                 
@@ -32,9 +53,9 @@ function output = myFun(input)
         
 
         %% Pre-Filtering CoP %%
-        for col = 4:2:6
+        for col = 3:4
             for row = 1:length(forcePlate1(:,1))
-                if 1 < contactTimeSteps(1) && contactTimeSteps(length(contactTimeSteps)) %% In the special case the contact only happens in mid movement, having no contact after and before
+                if 1 < contactTimeSteps(1) && contactTimeSteps(length(contactTimeSteps)) < row %% In the special case the contact only happens in mid movement, having no contact after and before
                     if ismember(row, contactTimeSteps) %%In the middle where contact happens
                         
                         forcePlate(row, col) = forcePlate(row, col);
@@ -75,21 +96,40 @@ function output = myFun(input)
     forcePlate2 = forcePlates{1, 2};
     forcePlate3 = forcePlates{1, 3};
     
+%     hold on;
+%     plot(forcePlate1(:,4),'DisplayName','forcePlate1')
+    
     [b10,a10] = butter(2,(2 * 10)/100,'low');
     [b20,a20] = butter(2,(2 * 20)/100,'low');
 
-    for col = 1:2:3
+    for col = 1:2
     
     forcePlate1(:,col) = filtfilt(b20,a20,forcePlate1(:,col));
     forcePlate1(:,col) = filtfilt(b20,a20,forcePlate1(:,col));
     forcePlate1(:,col) = filtfilt(b20,a20,forcePlate1(:,col));
+    forcePlate2(:,col) = filtfilt(b20,a20,forcePlate2(:,col));
+    forcePlate2(:,col) = filtfilt(b20,a20,forcePlate2(:,col));
+    forcePlate2(:,col) = filtfilt(b20,a20,forcePlate2(:,col));
+    forcePlate3(:,col) = filtfilt(b20,a20,forcePlate3(:,col));
+    forcePlate3(:,col) = filtfilt(b20,a20,forcePlate3(:,col));
+    forcePlate3(:,col) = filtfilt(b20,a20,forcePlate3(:,col));
 
     end
-    for col = 4:2:6
-    forcePlate1(:,col) = filtfilt(b10,a10,forcePlate1(:,col))/1000;
-    forcePlate1(:,col) = filtfilt(b10,a10,forcePlate1(:,col))/1000;
-    forcePlate1(:,col) = filtfilt(b10,a10,forcePlate1(:,col))/1000;
+    for col = 3:4
+    forcePlate1(:,col) = filtfilt(b10,a10,forcePlate1(:,col));
+    forcePlate1(:,col) = filtfilt(b10,a10,forcePlate1(:,col));
+    forcePlate1(:,col) = filtfilt(b10,a10,forcePlate1(:,col));
+    forcePlate2(:,col) = filtfilt(b10,a10,forcePlate2(:,col));
+    forcePlate2(:,col) = filtfilt(b10,a10,forcePlate2(:,col));
+    forcePlate2(:,col) = filtfilt(b10,a10,forcePlate2(:,col));
+    forcePlate3(:,col) = filtfilt(b10,a10,forcePlate3(:,col));
+    forcePlate3(:,col) = filtfilt(b10,a10,forcePlate3(:,col));
+    forcePlate3(:,col) = filtfilt(b10,a10,forcePlate3(:,col));
     end
+    
+%     plot(forcePlate1(:,4),'DisplayName','forcePlate1')
+%     
+%     hold off;
 
 
 end
